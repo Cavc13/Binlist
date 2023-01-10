2b402ac8ca1018842b38bdfb3da290f4fc914cb5
Konstantin Mikhailov <k.mikhailov@3205.team>
2023-01-10 20:36:35 +0300

    fix dagger hilt

--- app/src/main/java/com/snusnu/binlist/feature_binlist/presentation/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/snusnu/binlist/feature_binlist/presentation/dashboard/DashboardViewModel.kt
@@ -3,13 +3,14 @@ package com.snusnu.binlist.feature_binlist.presentation.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.snusnu.binlist.feature_binlist.data.repository.BinlistRepositoryImpl
+import com.snusnu.binlist.feature_binlist.domain.use_case.BinUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel @Inject constructor(
+    private val binUseCases: BinUseCases
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
--- app/src/main/java/com/snusnu/binlist/feature_binlist/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/snusnu/binlist/feature_binlist/presentation/home/HomeViewModel.kt
@@ -1,15 +1,13 @@
 package com.snusnu.binlist.feature_binlist.presentation.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.snusnu.binlist.feature_binlist.domain.use_case.BinUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val binUseCases: BinUseCases
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
